5c07fafaf749c3981b18b669deb5c38b518e4418
Ian Cullen <ian.james.cullen@gmail.com>
2021-02-11 19:46:28 +0000

    Additional NUBASE parse tests

--- tests/test_nubase_parse.py
+++ tests/test_nubase_parse.py
@@ -22,3 +22,60 @@ def test_read_halflife_error():
 
     line_02 = "025 0150   25P     18870#     200#                           <30     ns        1/2+#         00 93Po.Ai   p ?"
     assert parser._read_halflife_error(line_02) == None
+
+
+def test_readable_line():
+    parser = nbp.NubaseParser(".", 2003)
+
+    bad_line01 = "095 0420   95Mo   -87707.5      1.9                          stbl              5/2+          00           IS=15.92 13"
+    bad_line02 = "003 0030   3Li     28670#    2000#                      RN   p-unst                          98           p ?"
+    bad_line03 = "130 0556   130Csx -86873       17        27      15         R=.2~~~.1          fsmix"
+    bad_line04 = "267 1081   267Hsm                      non-exist        EU   200     ms                         95Ho.Atdi A=?;IT ?"
+
+    good_line = "072 0360   72Kr   -53941        8                             17.16   s 0.18   0+            95 03Pi03t   B+=100"
+
+    assert not parser._readable_line(bad_line01)
+    assert not parser._readable_line(bad_line02)
+    assert not parser._readable_line(bad_line03)
+    assert not parser._readable_line(bad_line04)
+
+    assert parser._readable_line(good_line)
+
+
+def test_read_line():
+    parser = nbp.NubaseParser(".", 2003)
+
+    iso_line = "183 0791   183Aum -30114       10        73.3     0.4         >1     us        (1/2)+        99           IT=100"
+    assert parser._read_line(iso_line) == dict()
+
+    gs_line = "057 0290   57Cu   -47310       16                            196.3   ms 0.7    3/2-          98           B+=100"
+    d = parser._read_line(gs_line)
+
+    assert d['A'] == 57
+    assert d['Z'] == 29
+    assert d['N'] == 28
+    assert d['Experimental'] is True
+    assert d['NubaseMassExcess'] == -47310.0
+    assert d['NubaseMassExcessError'] == 16.0
+    assert d['HalfLifeValue'] == 196.3
+    assert d['HalfLifeUnit'] == "ms"
+    assert d['HalfLifeError'] == 0.7
+    assert d['LevelSpin'] == "3/2-"
+    assert d['DiscoveryYear'] == 1900
+    assert d['Decay'] == "B+=100"
+
+    theoretical_gs_line = "110 0530   110I   -60320#     310#                           650     ms 20     1+#           00           B+=83 4;A=17 4;B+p=11 3;B+A=1.1 3"
+    d = parser._read_line(theoretical_gs_line)
+
+    assert d['A'] == 110
+    assert d['Z'] == 53
+    assert d['N'] == 57
+    assert d['Experimental'] is False
+    assert d['NubaseMassExcess'] == -60320
+    assert d['NubaseMassExcessError'] == 310
+    assert d['HalfLifeValue'] == 650
+    assert d['HalfLifeUnit'] == "ms"
+    assert d['HalfLifeError'] == 20
+    assert d['LevelSpin'] == "1+"
+    assert d['DiscoveryYear'] == 1900
+    assert d['Decay'] == "B+=83 4;A=17 4;B+p=11 3;B+A=1.1 3"
