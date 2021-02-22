117663106d47941ad03528582609758a4798c87d
Ian Cullen <ian.james.cullen@gmail.com>
2021-02-22 12:02:03 +0000

    Major change to the MassTable class so it contains all data
    The year is no longer passed in to get data from that year, but all
    years are read, the individual dataframes merged and the index set to
    the pubkished year. The NUBASE and AME frames are kept separate,
    perhaps we should merge them?

--- pynch/mass_table.py
+++ pynch/mass_table.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import pathlib
+import typing
 
 from pynch.nubase_parse import NubaseParser
 from pynch.ame_mass_parse import AMEMassParser
@@ -12,53 +14,90 @@ class MassTable:
     Internally there are separate dataframes for the NUBASE and AME data
     """
 
-    def __init__(self, year: int):
-        self.existing_years = [2003, 2012, 2016]
-        self.year = year
+    def __init__(self):
+        # Assume this file is some/path/pynch/pynch/mass_table.py
         self.data_path = pathlib.Path(__file__) / ".." / ".." / "data"
-        self._set_datafiles()
-        self._parse_datafiles()
-        self._merge_ame_data()
+        self.existing_years = [2003, 2012, 2016]
+        self.nubase = pd.concat([self._parse_nubase_data(y) for y in self.existing_years], ignore_index=True)
+        self.ame = pd.concat([self._parse_ame_data(y) for y in self.existing_years], ignore_index=True)
+        self._do_indexing()
 
-    def _set_datafiles(self):
+    def _get_nubase_datafile(self, year: int) -> str:
+        """
+        Use the given year to locate the nubase mass table file and return the absolute path.
+        """
+        nubase_mass = self.data_path / pathlib.Path(str(year))
+        nubase_mass = nubase_mass.resolve()
+
+        if year == 2003:
+            nubase_mass = nubase_mass / "nubtab03.asc"
+        elif year == 2012:
+            nubase_mass = nubase_mass / "nubtab12.asc"
+        elif year == 2016:
+            nubase_mass = nubase_mass / "nubase2016.txt"
+
+        return nubase_mass
+
+    def _get_ame_datafiles(self, year: int) -> typing.Tuple[str, str, str]:
+        """
+        Use the given year to locate the 3 AME data file and return the absolute path.
+        """
+        data_dir = self.data_path / pathlib.Path(str(year))
+        data_dir = data_dir.resolve()
+
+        if year == 2003:
+            ame_mass = data_dir / "mass.mas03"
+            ame_reaction_1 = data_dir / "rct1.mas03"
+            ame_reaction_2 = data_dir / "rct2.mas03"
+        elif year == 2012:
+            ame_mass = data_dir / "mass.mas12"
+            ame_reaction_1 = data_dir / "rct1.mas12"
+            ame_reaction_2 = data_dir / "rct2.mas12"
+        elif year == 2016:
+            ame_mass = data_dir / "mass16.txt"
+            ame_reaction_1 = data_dir / "rct1-16.txt"
+            ame_reaction_2 = data_dir / "rct2-16.txt"
+
+        return ame_mass, ame_reaction_1, ame_reaction_2
+
+    def _validate_year(self, year: int) -> None:
         """
         Point the appropriate variables at the required data files for the table year
         """
-        if self.year not in self.existing_years:
-            print(f"WARNING: {self.year} not a valid table year, using {self.existing_years[-1]}")
-            self.year = self.existing_years[-1]
+        if year not in self.existing_years:
+            print(f"WARNING: {year} not a valid table year, using {self.existing_years[-1]}")
+            year = self.existing_years[-1]
 
-        data_dir = self.data_path / pathlib.Path(str(self.year))
-        data_dir = data_dir.resolve()
+        return year
 
-        if self.year == 2003:
-            self.nubase_file = data_dir / "nubtab03.asc"
-            self.ame_mass_file = data_dir / "mass.mas03"
-            self.ame_reaction_1_file = data_dir / "rct1.mas03"
-            self.ame_reaction_2_file = data_dir / "rct2.mas03"
-        elif self.year == 2012:
-            self.nubase_file = data_dir / "nubtab12.asc"
-            self.ame_mass_file = data_dir / "mass.mas12"
-            self.ame_reaction_1_file = data_dir / "rct1.mas12"
-            self.ame_reaction_2_file = data_dir / "rct2.mas12"
-        elif self.year == 2016:
-            self.nubase_file = data_dir / "nubase2016.txt"
-            self.ame_mass_file = data_dir / "mass16.txt"
-            self.ame_reaction_1_file = data_dir / "rct1-16.txt"
-            self.ame_reaction_2_file = data_dir / "rct2-16.txt"
+    def _parse_nubase_data(self, year: int) -> pd.DataFrame:
+        """
+        Get the nubase for the given year as a pandas.DataFrame.
+        """
+        year = self._validate_year(year)
+        return NubaseParser(self._get_nubase_datafile(year), year).read_file()
 
-    def _parse_datafiles(self):
+    def _parse_ame_data(self, year: int) -> pd.DataFrame:
         """
-        Read the data from each file into it's own dataframe
+        Combine all the AME files from the given year into a pandas.DataFrame.
         """
-        self.nubase_df = NubaseParser(self.nubase_file, self.year).read_file()
-        self.ame_mass_df = AMEMassParser(self.ame_mass_file, self.year).read_file()
-        self.ame_reaction_1_df = AMEReactionParser_1(self.ame_reaction_1_file, self.year).read_file()
-        self.ame_reaction_2_df = AMEReactionParser_2(self.ame_reaction_2_file, self.year).read_file()
+        year = self._validate_year(year)
+        ame_mass, ame_reaction_1, ame_reaction_2 = self._get_ame_datafiles(year)
 
-    def _merge_ame_data(self):
+        ame_mass_df = AMEMassParser(ame_mass, year).read_file()
+
+        # Merge all 3 of the AME files/data frames into one
+        common_columns = ['A', 'Z', 'N', 'TableYear', 'Symbol']
+        temp_df = ame_mass_df.merge(AMEReactionParser_1(ame_reaction_1, year).read_file(), on=common_columns)
+        return temp_df.merge(AMEReactionParser_2(ame_reaction_2, year).read_file(), on=common_columns)
+
+    def _do_indexing(self) -> None:
         """
-        The AME data comes in 3 files, merge them into one dataframe
+        Set the index of the dataframe to the table year. This is done in place so nothing is returned.
+
+        param: Nothing
+
+        :return: Nothing
         """
-        df = self.ame_mass_df.merge(self.ame_reaction_1_df, on=['A', 'Z', 'N', 'TableYear'])
-        self.ame_df = df.merge(self.ame_reaction_2_df, on=['A', 'Z', 'N', 'TableYear'])
+        self.nubase.set_index("TableYear", inplace=True)
+        self.ame.set_index("TableYear", inplace=True)
